b4c86fc11b2f14a9a115c27d3a78eeb177ee7c9f
KL <laszlo.kiska@gmail.com>
2023-11-03 22:52:11 +0100

    Add loading/wainting/empty state for specialities section

--- src/lib/components/clinics/specialties.svelte
+++ src/lib/components/clinics/specialties.svelte
@@ -1,73 +1,98 @@
 <script lang="ts">
     import { Specialty } from "$lib/models/specialty";
     import { Api } from "$lib/utils/api";
-    import { Button } from "flowbite-svelte";
-    import { onMount } from "svelte";
+    import { Button, Spinner } from "flowbite-svelte";
     import Divider from "../Divider.svelte";
     import { PageMode, canEdit } from "$lib/enums/page_mode";
     import LL from "$lib/i18n/i18n-svelte";
     import Toggle from "../Toggle.svelte";
+    import SectionMessage from "../SectionMessage.svelte";
+    import sadFace from "$lib/images/sad_face.png";
+    import doge from "$lib/images/empty_doge.png";
 
     let pageMode: PageMode = PageMode.view;
     let specialties: Specialty[] = [];
-    let fetching: boolean = true;
 
     function toggle() {
         pageMode = pageMode == PageMode.edit ? PageMode.view : PageMode.edit;
     }
 
-    async function fetch() {
-        fetching = true;
-        try {
-            const json = await Api.requestBody<Map<string, string>[]>(
-                "GET",
-                "specialties"
-            );
-            json.forEach(function (item: Map<string, string>) {
-                const spec = Specialty.fromJson(item);
-                specialties.push(spec);
-            });
-            specialties = specialties;
-        } catch (error) {
-            console.log(error);
-        }
-        fetching = false;
+    async function getSpecialities(): Promise<Specialty[]> {
+        const json = await Api.requestBody<Map<string, string>[]>(
+            "GET",
+            "specialties"
+        );
+        json.forEach(function (item: Map<string, string>) {
+            const spec = Specialty.fromJson(item);
+            specialties.push(spec);
+        });
+        specialties = specialties;
+        return specialties;
     }
 
-    onMount(async () => {
-        await fetch();
-    });
+    let request = getSpecialities();
+
+    function refetch() {
+        request = getSpecialities();
+    }
 </script>
 
 <div class="flex items-center justify-between">
-    <h2 class="font-normal text-xl">Specialities</h2>
+    <h2 class="font-normal text-xl">{$LL.Specialities()}</h2>
     {#if canEdit(pageMode)}
         <div>
             <Button on:click={toggle}>
                 <i class="fa-solid fa-xmark text-white pr-2" />
-                Cancel
+                {$LL.Cancel()}
             </Button>
             <Button color="green">
                 <i class="fa-regular fa-floppy-disk text-white pr-2" />
-                Save
+                {$LL.Save()}
             </Button>
         </div>
     {:else}
         <Button on:click={toggle}>
             <i class="fa-solid fa-pen-to-square text-white pr-2" />
-            Edit
+            {$LL.Edit()}
         </Button>
     {/if}
 </div>
 <Divider />
-<div
-    class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4 gap-2"
->
-    {#each specialties as sp}
-        <div class="bg-white rounded-md">
-            <Toggle label={sp.name} disabled={!canEdit(pageMode)} />
+{#await request}
+    <div class="flex justify-center content-center w-full">
+        <div>
+            <Spinner size="10" color="blue" />
+            {$LL.Loading()}
+        </div>
+    </div>
+{:then array}
+    {#if array && array.length}
+        <div
+            class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4 gap-2"
+        >
+            {#each array as sp (sp.id)}
+                <div class="bg-white rounded-md">
+                    <Toggle label={sp.name} disabled={!canEdit(pageMode)} />
+                </div>
+            {/each}
         </div>
     {:else}
-        <div>no items to display</div>
-    {/each}
-</div>
+        <div>
+            <SectionMessage
+                imageUrl={doge}
+                imageAlt="empty_search_result"
+                message={$LL.EmptyResult()}
+                buttonLabel={$LL.Retry()}
+                buttonOnClick={refetch}
+            />
+        </div>
+    {/if}
+{:catch error}
+    <SectionMessage
+        imageUrl={sadFace}
+        imageAlt="sad_face"
+        message="{$LL.SomethingWentWrong()} ..."
+        buttonLabel={$LL.Retry()}
+        buttonOnClick={refetch}
+    />
+{/await}
